6e92aa5d95fc4ab9909f593b59e72c1cfbf10014
test <test@GestionIA.home>
2026-06-14 18:46:33 +0200

    Twig fonctionnel

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,11 @@
 
 namespace App\Controller;
 
+use App\Repository\TicketRepository;
+use App\Repository\IARepository;
+use App\Repository\EditeurRepository;
+use App\Repository\VersionRepository;
+use App\Repository\IngenieurRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,10 +14,25 @@ use Symfony\Component\Routing\Attribute\Route;
 final class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
-    {
+    public function index(
+        TicketRepository    $ticketRepo,
+        IARepository        $iaRepo,
+        EditeurRepository   $editeurRepo,
+        VersionRepository   $versionRepo,
+        IngenieurRepository $ingenieurRepo,
+    ): Response {
+        $allTickets = $ticketRepo->findAll();
+
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'tickets_total'      => count($allTickets),
+            'tickets_prioritaires' => count(array_filter($allTickets, fn($t) => $t->isPriorite())),
+            'tickets_normaux'    => count(array_filter($allTickets, fn($t) => !$t->isPriorite())),
+            'tickets_recents'    => array_slice($allTickets, 0, 5),
+            'ias'                => $iaRepo->findAll(),
+            'nb_ia'              => count($iaRepo->findAll()),
+            'nb_editeurs'        => count($editeurRepo->findAll()),
+            'versions_recentes'  => array_slice($versionRepo->findAll(), 0, 5),
+            'ingenieurs'         => $ingenieurRepo->findAll(),
         ]);
     }
 }
